5ddf8c8aefd1e112d5cc627fe59b36c5b48814e8
jnyjhow <jhonatan_yamane@hotmail.com>
2024-07-15 19:14:26 -0300

    feat: using the Tailwind CLI create a new tailwind.config.js file.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/templates/*.html", 
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
